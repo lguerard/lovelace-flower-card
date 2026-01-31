e416e69cf16633f4b15533c102ddb3d9c44944ab
Laurent Guerard <laurent@guerard.email>
2026-01-31 13:55:35 +0100

    fix(types): revert PlantInfo transition to any for build compatibility

--- src/types/flower-card-types.ts
+++ src/types/flower-card-types.ts
@@ -38,7 +38,7 @@ export interface HomeAssistantEntity extends HassEntity {
 }
 
 export interface PlantInfo {
-  result: Record<string, PlantAttribute | string | number | undefined>;
+  result: any;
 }
 
 export interface PlantAttribute {
